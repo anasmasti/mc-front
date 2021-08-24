8db8cf111d7d6fbf606396a1dedb4583ba4bac23
anasmasti <anasmasti@hotmail.com>
2021-08-24 22:48:34 +0100

    Setup state management (Redux)

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
-import '../styles/globals.scss'
-import 'tailwindcss/tailwind.css'
-import MainLayout from '../layouts/MainLayout'
+import "../styles/globals.scss";
+import "tailwindcss/tailwind.css";
+import MainLayout from "../layouts/MainLayout";
+import { Provider } from "react-redux";
+import { useStore } from "../store/store";
 
 function MCApp({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState);
+
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
-  )
+    <Provider store={store}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </Provider>
+  );
 }
 
-export default MCApp
+export default MCApp;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
-import Link from "next/link";
 import homeStyle from "../styles/pages/Home.module.scss";
 import Landing from "../components/shared/landing/Landing";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { incrementCount } from "../store/actions";
 
 export default function Home() {
+  // const dispatch = useDispatch();
+  // const count = useSelector((state) => state.counter.count);
+  
+  // const getCount = () => {
+  //   dispatch(incrementCount());
+  //   console.log(count);
+  // }
+
+
   return (
     <div className={[homeStyle.home_section].join(" ")}>
       <div>
+        {/* <h1>{ count }</h1>
+        <button onClick={getCount}>Get Count</button> */}
         <Landing />
       </div>
     </div>
